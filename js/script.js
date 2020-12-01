e3d7565c00f8dcfd0b97bc23bcedd559e84fce03
Dom Siravo <domcode88@gmail.com>
2020-12-01 23:46:32 +0000

    Done.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"> </script>
+  <script>
+   $("#text").append("this is so freaking cool.");
+  </script>})
